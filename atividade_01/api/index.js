60bf37fa6020b078f00cfcdf26905119317db3df
Luana Cabral <163360766+luanacabral05@users.noreply.github.com>
2026-03-12 22:02:16 +0000

    Adicionando express + get / + utilização da variável PORT

---
+++ atividade_01/api/index.js
@@ -0,0 +1,22 @@
+import "dotenv/config";
+import cors from "cors";
+import express from "express";
+
+const app = express();
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send(
+    "Received a GET HTTP method\nServidor rodando!\n" + process.env.MESSAGE,
+  );
+});
+
+const port = process.env.PORT ?? 3000;
+
+app.listen(port, () =>
+  console.log(
+    "Example app listening on port " + port + "!\n" + process.env.MESSAGE,
+  ),
+);
+
+export default app;
